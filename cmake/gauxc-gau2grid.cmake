cbc377c191e1159540d8ed923338cb849ae090ee
asadchev <asadchev@gmail.com>
2021-01-13 16:51:07 -0500

    Stop gau2grid setting CMAKE_INSTALL_PREFIX to "/usr/local/gau2grid"

--- cmake/gauxc-gau2grid.cmake
+++ cmake/gauxc-gau2grid.cmake
@@ -9,20 +9,21 @@ if( NOT gau2grid_FOUND )
     gau2grid
     GIT_REPOSITORY https://github.com/dgasmith/gau2grid.git
     GIT_TAG 9c3d39c109747e80c7ff1ababbb36b80823ff5b5
-  )
+    )
 
   set( MAX_AM 6 CACHE STRING "" )
   FetchContent_GetProperties( gau2grid )
   if( NOT gau2grid_POPULATED )
     FetchContent_Populate( gau2grid )
+    # rewrite gau2grid/CMakeLists.txt
     file( READ ${gau2grid_SOURCE_DIR}/CMakeLists.txt GAU2GRID_CMAKE_LISTS )
-    string( REPLACE "CMAKE_SOURCE_DIR" "PROJECT_SOURCE_DIR" GAU2GRID_CORRECT "${GAU2GRID_CMAKE_LISTS}" )
-    file( WRITE ${gau2grid_SOURCE_DIR}/CMakeLists.txt "${GAU2GRID_CORRECT}" )
+    string( REPLACE "CMAKE_SOURCE_DIR" "PROJECT_SOURCE_DIR" GAU2GRID_CMAKE_LISTS "${GAU2GRID_CMAKE_LISTS}" )
+    # why fuck library sets CMAKE_INSTALL_PREFIX?!
+    string(REGEX REPLACE "[^\#]set\\(CMAKE_INSTALL_PREFIX" "#set(CMAKE_INSTALL_PREFIX" GAU2GRID_CMAKE_LISTS "${GAU2GRID_CMAKE_LISTS}" )
+    file( WRITE ${gau2grid_SOURCE_DIR}/CMakeLists.txt "${GAU2GRID_CMAKE_LISTS}" )
     add_subdirectory( ${gau2grid_SOURCE_DIR} ${gau2grid_BINARY_DIR} )
     target_compile_definitions( gg PRIVATE $<BUILD_INTERFACE:__GG_NO_PRAGMA> )
   endif()
-
-
 
   target_link_libraries( gauxc_gau2grid INTERFACE gg )
   target_include_directories( gauxc_gau2grid INTERFACE $<BUILD_INTERFACE:${gau2grid_BINARY_DIR}> )
